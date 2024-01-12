ba47136cede63edf35d9e73300fd95d4aabb1d89
Martynas Bielskus <61162271+martis99@users.noreply.github.com>
2024-01-12 17:35:08 +0200

    delete old code

--- scripts/xampp/start-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set "toolsPath=%CD%\tools"
-
-set "xamppPath=%toolsPath%\xampp"
-
-start %xamppPath%\apache_start.bat
-start %xamppPath%\mysql_start.bat
--- scripts/xampp/stop-dev.bat
+++
@@ -1,9 +0,0 @@
-rem @echo off
-setlocal enabledelayedexpansion
-
-set "toolsPath=%CD%\tools"
-
-set "xamppPath=%toolsPath%\xampp"
-
-start %xamppPath%\apache_stop.bat
-start %xamppPath%\mysql_stop.bat
